f8bf1a12edc7aef584551a1205bbe1207a67e85d
sanginiladia <43060844+sanginiladia@users.noreply.github.com>
2018-11-02 14:57:14 +0530

    Update fcfs_prog1.cpp

--- fcfs_prog1.cpp
+++ fcfs_prog1.cpp
@@ -1,47 +1,30 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
 int main()
-{   
-     cout<<"\t\t\t\tFIRST COME FIRST SERVE FOR ZERO ARRIVAL TIME\n";
-    int n;
-    cout<<"Enter no. of processes:";
-    cin>>n;                
-	int BT[n]; 
-	cout<<"Enter Burst time:\n";
-    for(int i=0;i<n;i++)  
-    {
-      cin>>BT[i];
-    }
-    int CT[n],TAT[n],WT[n];
-    
-    for(int i=0;i<n;i++)
-    {   
-        if(i==0)
-        {
-        	CT[0]=BT[0];
-        	TAT[0]=CT[0];
-        	WT[0]=TAT[0]-BT[0];
-		}
-        else
-        {
-    	CT[i]=CT[i-1]+BT[i];  
-			TAT[i]=CT[i];         
-		WT[i]=TAT[i]-BT[i];  
-		}
-     }
-	cout<<"CT  TAT  WT"<<endl ;
-	for(int i=0;i<n;i++)  
+{
+	int i,b[10],n;
+	float tat,w=0,sum=0;
+	cout<<"Enter number of processes: ";
+	cin>>n;
+	for(i=1;i<=n;i++)
 	{
-		cout<<CT[i]<<"   "<<TAT[i]<<"   "<<WT[i]<<endl;
+		cout<<"Burst time of "<<i<<" process: ";
+		cin>>b[i];
+		tat=b[1];
 	}
-	float sum1=0,sum2=0;
-	for(int i=0;i<n;i++) 
-	{
-		sum1=sum1+TAT[i];
-		sum2=sum2+WT[i];
+	
+	cout<<"\n\tProcess \t   BT    \t Waiting time \t turn around time\n";
+	for(i=1;i<=n;i++)
+	{	
+		tat=b[i]+w;
+		cout<<"Process   "<<i<<"  \t\t   "<<b[i]<<"  \t\t  "<<w<<"  \t\t "<<tat<<endl;
+		w+=b[i];
+		sum=sum+tat;
 	}
-	cout<<"Average TAT:"<<sum1/n<<endl;
-	cout<<"Average WT:"<<sum2/n;
-    return 0;    
+	
+	cout<<"Average waiting time : "<<tat/n<<endl;
+	cout<<"Average TAT :"<<sum/n;
+	return 0;
 }
+
+
